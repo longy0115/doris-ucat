0bfc20d05e63a518cb0bbca2de72da8bba4ed98f
zhenlong.zhang <longy0115@163.com>
2019-07-04 23:42:23 +0800

    添加 部署脚本

---
+++ apihooks.php
@@ -0,0 +1,16 @@
+<?php
+// 与webhook配置相同，为了安全，请设置此参数
+$secret = "doris-ucat";
+// 项目路径
+$path = "/home/dnmp/www/doris-ucat";
+// 校验发送位置，正确的情况下自动拉取代码，实现自动部署
+$signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
+if ($signature) {
+    $hash = "sha1=" . hash_hmac('sha1', file_get_contents("php://input"), $secret);
+    if (strcmp($signature, $hash) == 0) {
+        echo shell_exec("cd \ && cd {$path} && git pull 2>&1");
+        exit();
+    }
+}
+http_response_code(404);
+
