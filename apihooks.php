fcf3fe4de1e21413a2e62f29557a38f773c5525d
zhenlong.zhang <longy0115@163.com>
2019-07-05 18:42:24 +0800

    修改 脚本

--- apihooks.php
+++ apihooks.php
@@ -1,72 +1,17 @@
 <?php
+// GitHub Webhook Secret.
+// Keep it the same with the 'Secret' field on your Webhooks / Manage webhook page of your respostory.
+$secret = "doris-ucat";
+// Path to your respostory on your server.
+// e.g. "/var/www/respostory"
 $path = "/home/dnmp/www/doris-ucat";
-$cmd = "cd $path && git pull origin master";
-exec("cd $path && git pull origin master");
-exit(111);
-go();
-function go()
-{
-    // webhook上设置的secret
-    $secret = "doris-ucat";
-
-    // 校验发送位置，正确的情况下自动拉取代码，实现自动部署
-    $signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
-    if ($signature) {
-        $hash = "sha1=" . hash_hmac('sha1', file_get_contents("php://input"), $secret);
-        if (strcmp($signature, $hash) == 0) {
-            // sign sucess
-
-            set_time_limit(3 * 60); //最大过期时间3分钟
-            $path = "/home/dnmp/www/doris-ucat";
-            $cmd = "cd $path && git pull origin master";
-            exec("cd $path && git pull origin master");
-            $res=exec($cmd,$output,$status);
-
-            $res_log = '-------------------------' . PHP_EOL;
-            $res_log .= json_encode($output) . '--';
-            $res_log .= $cmd;
-            $res_log .= '--' . $status . '--';
-            $res_log .= $res . PHP_EOL;
-            file_put_contents("apihooks.txt", $res_log, FILE_APPEND);//追加写入
-            print_r($res);
-            // $res = doShell($cmd);
-            // print_r($res); // 主要打印结果给github记录查看，自己测试时查看
-
-        }
+// Headers deliveried from GitHub
+$signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
+if ($signature) {
+    $hash = "sha1=" . hash_hmac('sha1', $HTTP_RAW_POST_DATA, $secret);
+    if (strcmp($signature, $hash) == 0) {
+        echo shell_exec("cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1");
+        exit();
     }
 }
-
-/*
- * 执行shell命令
- */
-function doShell($cmd, $cwd = null)
-{
-    $descriptorspec = array(
-        0 => array("pipe", "r"), // stdin
-        1 => array("pipe", "w"), // stdout
-        2 => array("pipe", "w"), // stderr
-    );
-    $proc = proc_open($cmd, $descriptorspec, $pipes, $cwd, null);
-    // $proc为false，表明命令执行失败
-    if ($proc == false) {
-        // return false;
-        // do sth with HTTP response
-        print_r("命令执行出错！");
-    } else {
-        $stdout = stream_get_contents($pipes[1]);
-        fclose($pipes[1]);
-        $stderr = stream_get_contents($pipes[2]);
-        fclose($pipes[2]);
-        $status = proc_close($proc); // 释放proc
-    }
-    $data = array(
-        'cmd'    =>$cmd,
-        'stdout' => $stdout, // 标准输出
-        'stderr' => $stderr, // 错误输出
-        'retval' => $status, // 返回值
-    );
-    return $data;
-}
-
-
-
+http_response_code(404);
