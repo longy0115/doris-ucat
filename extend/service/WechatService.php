3531cd251abf5a6fbf98a0e2c087d86fd5bc2f6a
zhenlong.zhang <longy0115@163.com>
2019-07-16 20:58:34 +0800

    update

--- extend/service/WechatService.php
+++ extend/service/WechatService.php
@@ -406,7 +406,7 @@ class WechatService
         $jsService = self::jsService();
         if($url) $jsService->setUrl($url);
         try{
-            return $jsService->config($apiList,$debug);
+            return $jsService->config($apiList,$debug,false,false);
         }catch (\Exception $e){
             return '{}';
         }
