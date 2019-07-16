81f5e3630e1162e694c81dda1450b5678c7dd3df
zhenlong.zhang <longy0115@163.com>
2019-07-16 12:42:16 +0800

    添加路由

--- application/route.php
+++ application/route.php
@@ -45,4 +45,5 @@ Route::post('register','Users/register');
 
 //微信公众号
 Route::get('wechat', 'wechat/Wechat/index');
-Route::get('reply', 'wechat/Wechat/reply');//验证入口
+Route::get('reply', 'wechat/Wechat/reply'); //验证入口
+Route::get('wechat/Wechat/reply', 'wechat/Wechat/reply');//验证入口
